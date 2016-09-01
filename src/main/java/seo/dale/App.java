07bd759d85669785d7457ffb4a71b7a83acb517a
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-09-01 10:26:33 +0900

    Initial commit

---
+++ src/main/java/seo/dale/App.java
@@ -0,0 +1,13 @@
+package seo.dale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
